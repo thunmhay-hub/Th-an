43597eb39808804bbae2e640ee108e800c576e21
thunmhay-hub <thunmhay@gmail.com>
2025-08-20 08:06:49 +0700

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -10,7 +10,7 @@ MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-bot = Bot(API_TOKEN)
+bot = Bot(8333481006:AAFNGkXsr-HYRAoAKzTCBldYLyouZw-Paxs)
 
 def start_bot():
     """Start the bot script as a subprocess."""
